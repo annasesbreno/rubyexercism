1d25f925f95c6932ce60a2fd26eb5f50a5668b2d
Your Name <your.email@example.com>
2017-08-24 16:16:51 +0800

    WUIP Milestone 4

--- petshop-master/app/controllers/animals_controller.rb
+++ petshop-master/app/controllers/animals_controller.rb
@@ -1,6 +1,5 @@
 class AnimalsController < ApplicationController
-  before_action :set_animal, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_animal, only: [:show, :edit, :update, :destroy, :change_status_sale, :change_status_sold, :species, :total_amount_sale]
   # GET /animals
   # GET /animals.json
   def index
@@ -10,6 +9,7 @@ class AnimalsController < ApplicationController
   # GET /animals/1
   # GET /animals/1.json
   def show
+    @animal = Animal.find(params[:id])
   end
 
   # GET /animals/new
@@ -28,7 +28,7 @@ class AnimalsController < ApplicationController
 
     respond_to do |format|
       if @animal.save
-        format.html { redirect_to @animal, notice: 'Animal was successfully created.' }
+        format.html { redirect_to @animal}
         format.json { render :show, status: :created, location: @animal }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class AnimalsController < ApplicationController
   def update
     respond_to do |format|
       if @animal.update(animal_params)
-        format.html { redirect_to @animal, notice: 'Animal was successfully updated.' }
+        format.html { redirect_to @animal}
         format.json { render :show, status: :ok, location: @animal }
       else
         format.html { render :edit }
@@ -51,22 +51,58 @@ class AnimalsController < ApplicationController
     end
   end
 
+  def change_status_sale
+      @animal.Status = "For Sale"
+      @animal.save
+      redirect_to animals_path  
+  end
+
+  def change_status_sold
+      @animal.Status = "Sold"
+      @animal.save
+      redirect_to animals_path  
+  end
+
+  def show_inventory
+    @animals_for_sale = Animal.where(Status: 'For Sale').group_by(&:Species)
+    @animals_for_sold = Animal.where(Status: 'Sold').group_by(&:Species)
+  end
+
+  def show_species
+    @animals_species_sale = Animal.where(Status: 'For Sale').group_by(&:Species)
+    @animals_species_sold = Animal.where(Status: 'Sold').group_by(&:Species)
+  end
+
+  def show_breed
+    @animals_breed_sale = Animal.where(Status: 'For Sale').group_by(&:Breed)
+    @animals_breed_sold = Animal.where(Status: 'Sold').group_by(&:Breed)
+  end
+
+  def show_status
+    @animals_for_sale = Animal.where(Status: 'For Sale').group_by(&:Species)
+    @animals_for_sold = Animal.where(Status: 'Sold').group_by(&:Species)
+  end
+
+  def total_amount_sale
+    @animal = Animal.sum(&:Price)
+  end
+
   # DELETE /animals/1
   # DELETE /animals/1.json
   def destroy
     @animal.destroy
     respond_to do |format|
-      format.html { redirect_to animals_url, notice: 'Animal was successfully deleted.' }
+      format.html { redirect_to animals_url}
       format.json { head :no_content }
     end
   end
 
-
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_animal
-      @animal = Animal.find(params[:id])
-    end
+  def set_animal
+    id = params[:animal_id] || params[:id]
+    @animal = Animal.find(id)
+  end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def animal_params
--- petshop-master/config/routes.rb
+++ petshop-master/config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
-  resources :animals
-  root to: redirect('/animals')
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'user/index'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  resources :users
+    get 'show_inventory' => 'animals#show_inventory'
+    get 'show_species' => 'animals#show_species'
+    get 'show_breed' => 'animals#show_breed'
+    get 'show_status' => 'animals#show_status'
+  resources :animals do
+    get 'change_status_sale' => 'animals#change_status_sale'
+    get 'change_status_sold' => 'animals#change_status_sold'
+    get 'species' => 'animals#species'
+    get 'total_amount_sale' => 'animals#total_amount_sale'
+    end
 end
+
